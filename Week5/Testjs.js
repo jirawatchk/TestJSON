60ea289fccfde41d440855f36c8780c0051b1087
MrGolf2022 <jirawat.ch@tnk.ac.th>
2022-11-17 08:31:58 +0700

    Delete Testjs.js

--- Week5/Testjs.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function(requset, respone) {
-    respone.writeHead(200, { 'Content-Type': 'text/pain' });
-    respone.end("Hello World");
-
-}).listen(8001, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8001/')
