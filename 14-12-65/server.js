c2e8266268c27e72db9e9ea6bf13f7f81f50b281
MrGolf2022 <jirawat.ch@tnk.ac.th>
2022-12-15 10:26:45 +0700

    ok

--- 14-12-65/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
-app.listen(4200,()=>{
-   console.log('Start server at port 4200'); 
-})
